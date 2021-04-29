89f55b1c3aae271c46e63517a77f4b62b5fc9580
Prajwal Nagrale <prajwalnagrale@gmail.com>
2021-04-29 11:22:49 +0530

    first commit

---
+++ src/Components/register/RegisterComponent.js
@@ -0,0 +1,48 @@
+import './register.css'
+
+function RegisterComponent(){
+
+
+  
+    return(
+        <section>
+        <div class="register">
+                <div class="createAccount">
+                    <form name="form" class="userForm">
+                        <h2>Sign Up</h2> 
+                        <h6>It's free and always wil be.</h6>
+                        <table class="center">
+                          <tr>
+                            <td><label for="name">Name:</label></td>
+                            <td><input type="text" name="name"   /></td>
+                          </tr>
+                          <tr><td></td>
+                            </tr>
+                          <tr>
+                            <td><label for="email">Email:</label></td>
+                            <td><input type="text"  name="email" class="form-control" formControlName="email"  /></td>
+                          </tr>
+                          <tr>
+                            <td></td>
+                              
+                            </tr>
+                            <tr>
+                              <td><label for="password">Password:</label></td>
+                              <td><input type="password"  name="password"  class="form-control" formControlName="password" /></td>
+                            </tr>
+                            <tr>
+                              <td></td>
+                              
+                            </tr>
+                            <tr></tr>
+                        </table>
+                        <p><button type="submit"  >Create Account</button></p>
+                    </form>
+                </div>
+
+        </div>
+    </section>
+    );
+}
+
+export default RegisterComponent;
